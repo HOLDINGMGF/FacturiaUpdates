798075b90c5d0bd26f561604ec9b5a0c33a72fa7
Jimmy RIVET <j.rivet@MGF.local>
2026-07-17 17:04:52 +0200

    Publish Facturia patch 0.6.70-test

--- tools/FacturiaManualPatcher.ps1
+++ tools/FacturiaManualPatcher.ps1
@@ -3,8 +3,8 @@ Add-Type -AssemblyName System.Drawing
 
 $ErrorActionPreference = "Stop"
 
-$DefaultPatchUrl = "https://raw.githubusercontent.com/HOLDINGMGF/FacturiaUpdates/main/update-test/facturia-patch-0.6.69-test.zip"
-$DefaultVersion = "0.6.69-test"
+$DefaultPatchUrl = "https://raw.githubusercontent.com/HOLDINGMGF/FacturiaUpdates/main/update-test/facturia-patch-0.6.70-test.zip"
+$DefaultVersion = "0.6.70-test"
 
 function Test-IsAdministrator {
     $identity = [Security.Principal.WindowsIdentity]::GetCurrent()
